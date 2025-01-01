34ace14924885fc395f0f817eddb4fd72822b569
brianwu <fslikeyou1033@yahoo.com>
2025-01-01 15:33:59 -0500

    add nav

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,9 +2,9 @@ import "./Navbar.scss";
 
 function Navbar() {
     return (
-        <>
-            
-        </>
+        <div className="nav">
+            <h1 className="nav__wordmark">Timence</h1>
+        </div>
     )
 };
 
